e21d8e716d484cb39e8b2c25d0b03571834624f8
I-Create-Portfolio <gordwincreative@gmail.com>
2025-03-20 18:59:46 +0300

    Add files via upload

---
+++ discover/privacy-policy.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Privacy STATEMENT | Everyday Kibera.Net</title>
+
+    <!-- favicon -->
+    <link href="assets/img/favicon.png" rel=icon>
+
+    <!-- web-fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,500' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- font-awesome -->
+    <link href="assets/fonts/font-awesome/font-awesome.min.css" rel="stylesheet">
+    <!-- Mobile Menu Style -->
+    <link href="assets/css/mobile-menu.css" rel="stylesheet">
+
+    <!-- Owl carousel -->
+    <link href="assets/css/owl.carousel.css" rel="stylesheet">
+    <link href="assets/css/owl.theme.default.min.css" rel="stylesheet">
+    <!-- Theme Style -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar">
+<div id="main-wrapper">
+<!-- Page Preloader -->
+<div id="preloader">
+    <div id="status">
+        <div class="status-mes"></div>
+    </div>
+</div>
+<!-- preloader -->
+
+<div class="uc-mobile-menu-pusher">
+
+<?php include ("includes/header.php"); ?>
+
+<div id="container1">
+  <div class="content-wrapper">
+    <div id="content">
+
+        <h4> 
+            <H1>EVERYDAYKIBERA.NET PRIVACY STATEMENT</H1>
+        </h4>
+        <P>By using this website, you are consenting to our collection and use of information in accordance with this 
+            Privacy Policy.</P>
+        <button type="button" class="collapsible">What information do we gather about you?</button>
+    <div class="content">
+            <P>
+                We and our third-party collaborators collect certain information regarding your use of Everyday Kibera.Net, such as your 
+                IP address and browser type. Your session and the pages you visit on everyday Kibera.Net might be stored, but you will
+                remain anonymous. We may use your IP address to identify the general geographic area from which you are
+                accessing Everyday Kibera.Net. We connect data from different systems but do not link IP addresses to any personal 
+                information. We may also collect other information as described in this policy.
+            </P>
+            <P>
+            </P>
+            </div>
+
+        <button type="button" class="collapsible">
+        1. What Information Do We Gather About You?</button>
+            <div class="content">
+             <p>
+             <h2 class="h-default is-style-h3 wp-block-heading" id="h-what-do-we-use-your-information-for">What do we use your information for?</h2>
+                <p>We use the information we gather from you for systems administration purposes, abuse prevention, and to track user trends, and for the other purposes described in this policy. If you send us an email, the email address you provide may be used to send you information, respond to inquiries, and/or other requests or questions. 
+                    We will not share, sell, rent, swap, or authorize any third party to use your email address for commercial purposes without your permission.</p>
+                <p>User information may be shared with third-party vendors to the extent necessary to provide and improve web services or other communications to users. 
+                    For example, we use third parties such as Chartbeat and Google Analytics to generate reports on site usage, web traffic, user behavior, and user 
+                    interests in order to optimize our website for our visitors. We also use geographic, demographic, and interest-based reports of our website visitors to create custom audience lists. 
+                    We prohibit any third parties who receive user information for this purpose from using or sharing user information for any purpose other than providing services for the benefit of our users.</p>
+                <p>We may also provide your information to third parties in circumstances where we believe that doing so is necessary or appropriate to satisfy any applicable law, regulation, legal process or 
+                    governmental request; detect, prevent or otherwise address fraud, security, or technical issues; or protect our rights and safety and the rights and safety of our users or others.</p>
+                
+                <h2 class="h-default is-style-h3 wp-block-heading" id="h-how-is-this-information-collected-and-how-can-you-opt-out">How is this information collected and how can you opt out?</h2>
+                <p>Google and other third parties may use cookies, web beacons, and similar technologies to collect or receive information from this website and elsewhere on the internet and use that information 
+                    to provide measurement services and target ads. For more information on Google Analytics, consult their <a href="http://www.google.com/intl/en/policies/terms/">terms of use</a>, <a href="https://privacy.google.com/">privacy practices</a>, and <a href="https://adssettings.google.com/authenticated">ads settings</a>. 
+                You can opt out of the collection and use of this information through tools like the <a href="http://www.networkadvertising.org/managing/opt_out.asp">Network Advertising Initiative opt-out page</a>.</p>
+
+
+
+<h2 class="h-default is-style-h3 wp-block-heading" id="h-cookies">Cookies</h2>
+<p>Cookies are small files that are stored on your computer (unless you block them). We use cookies to understand and save your preferences for future visits and compile aggregate data about site traffic and site interaction so that we can offer better site experiences and tools in the future. You may disable cookies through your individual browser options or you can opt out of the collection and use of this information through tools like the <a href="http://www.networkadvertising.org/managing/opt_out.asp">Network Advertising Initiative opt-out page</a>.</p>
+<h2 class="h-default is-style-h3 wp-block-heading" id="h-other-websites-and-cookies">Other websites and cookies</h2>
+<p>This website may contain links to other websites. We are not responsible for the privacy practices or the content of such websites.</p>
+<p>Cookies may be set by parties other than us. These “third-party cookies” may, for example, originate from websites such as YouTube, Twitter, Facebook, Soundcloud, or other social media services for which Harvard.edu has implemented “plug-ins.” Since the cookie policies of these sites change over time, you should determine their policies by visiting the privacy policy pages of these sites directly.</p>
+<h2 class="h-default is-style-h3 wp-block-heading" id="h-information-protection">Information protection</h2>
+<p>This site has reasonable security measures in place to help protect against the loss, misuse, and alteration of the information under our control. However, no method of transmission over the Internet or method of electronic storage is 100% secure.</p>
+<p>This Privacy Policy may be amended from time to time. Any such changes will be posted on this page.</p>
+<h2 class="h-default is-style-h3 wp-block-heading" id="h-effective-date">Effective date</h2>
+<p>The effective date of this policy is&nbsp;MARCH 7, 2025.</p>
+</div></div>
+
+    </div>
+    <div class="category_article_wrapper"></div>
+  </div>
+
+<?php include ("includes/footer.php"); ?>
+
+</div>
+
+
+<!-- #main-wrapper -->
+
+<!-- jquery Core-->
+<script src="assets/js/jquery-2.1.4.min.js"></script>
+
+<!-- Bootstrap -->
+<script src="assets/js/bootstrap.min.js"></script>
+
+<!-- Theme Menu -->
+<script src="assets/js/mobile-menu.js"></script>
+
+<!-- Owl carousel -->
+<script src="assets/js/owl.carousel.min.js"></script>
+
+<!-- Theme Script -->
+<script src="assets/js/script.js"></script>
+</body>
+
+</html>
